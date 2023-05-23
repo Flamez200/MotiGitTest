bd58125554a1f9807905e2dc19380e0abebe154f
Shuajb <manushishuajb@gmail.com>
2023-05-23 21:21:22 +0200

    scroll animation change/fix

--- js/app.js
+++ js/app.js
@@ -13,4 +13,10 @@
    const hiddenElements = document.querySelectorAll('.hidden');
    hiddenElements.forEach((el) => observer.observe(el));
 
+   let progress = document.getElementById('progressbar');
+   let totalHeight = document.body.scrollHeight - window.innerHeight;
+   window.onscroll = function(){
+     let progressHeight = (window.pageYOffset / totalHeight) * 100;
+     progress.style.height = progressHeight + "%";
+   }
    
